d9a55f373c4a0da910f04783c7a37bc6a9a0263a
Oluwasayo Akinkunmi <Oluwasayo.Akinkunmi@interswitchng.com>
2026-06-19 13:24:28 +0100

    add k8s and healthcheck

---
+++ Demo.Payment/src/Demo.Payment.Service/HealthChecks/SlowDependencyHealthCheck.cs
@@ -0,0 +1,23 @@
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+
+namespace Demo.Payments.Service.HealthChecks;
+
+public class SlowDependencyHealthCheck : IHealthCheck
+{
+    public static readonly string HealthCheckName = "slow_dependency";
+    private static int invocationCount = 0;
+
+    public SlowDependencyHealthCheck() { }
+    public Task<HealthCheckResult> CheckHealthAsync(HealthCheckContext context, CancellationToken cancellationToken = default)
+    {
+        if (invocationCount++ > 3)
+        {
+            return Task.FromResult(HealthCheckResult.Healthy($"Dependency is ready"));
+        }
+
+        return Task.FromResult(new HealthCheckResult(
+            status: context.Registration.FailureStatus,
+            description: $"Dependency is still initializing. Invocation count: {invocationCount}"
+        ));
+    }
+}
